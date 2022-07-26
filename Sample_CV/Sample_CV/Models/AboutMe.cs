e1f767b443bbd0bafb49d09a915416d8de37276e
marziehfatemi@gmail.com <marziehfatemi@gmail.com>
2022-07-26 13:17:48 +0430

    MyInfo View component as header added

---
+++ Sample_CV/Sample_CV/Models/AboutMe.cs
@@ -0,0 +1,11 @@
+﻿namespace Sample_CV.Models
+{
+    public class AboutMe
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string JobTitle { get; set; }
+        public string[] Products { get; set; }
+        public string Image { get; set; }
+    }
+}
